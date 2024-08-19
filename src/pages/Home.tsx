3462cee7fe33ecca2a18cc587b54c43f62b362e7
yugyusang <yugyusang12@gmail.com>
2024-08-19 23:03:21 +0900

    feat:home페이지 지도 구현중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,87 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserDataStore } from "../utils/stores/userStore";
+import Map, { NavigationControl, useMap } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import styled from "styled-components";
+
 const Home = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const [location, setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
+  const { userName } = useUserDataStore((state) => ({
+    userName: state.userName,
+  }));
+
+  //로그인이 되어있지 않다면 로그인 페이지로 이동
+  useEffect(() => {
+    if (!userName) {
+      navigate("/login");
+    }
+  }, [navigate, userName]);
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error Code = " + error.code + " - " + error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <>
+      <Map
+        reuseMaps
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        initialViewState={{
+          latitude: location?.latitude ?? 37.5326,
+          longitude: location?.longitude ?? 127.024612,
+          zoom: 14,
+        }}
+        // onMove={(event) => {setViewState(event.viewState)}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <NavigationControl position="top-right" />
+        <TestMapButton location={location} />
+      </Map>
+    </>
+  );
 };
 
 export default Home;
+
+const TestButtons = styled.button`
+  position: absolute;
+  z-index: 1;
+`;
+
+type TestMapButtonProps = {
+  location: { latitude: number; longitude: number } | null;
+};
+const TestMapButton = ({ location }: TestMapButtonProps) => {
+  const { current: map } = useMap();
+  const onClickHandler = () => {
+    if (location) {
+      map?.flyTo({ center: [location.longitude, location.latitude] });
+    }
+  };
+
+  useEffect(() => {
+    if (location) {
+      map?.flyTo({ center: [location.longitude, location.latitude] });
+    }
+  }, [location, map]);
+
+  return <TestButtons onClick={onClickHandler}>현재 위치로</TestButtons>;
+};
