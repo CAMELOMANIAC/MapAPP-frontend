01c917ad499226da1528e9e978c6343a95826277
yugyusang <yugyusang12@gmail.com>
2024-09-03 23:24:03 +0900

    fix: 로그인 페이지 문구수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { Link, useOutletContext } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { Input, Label } from "../assets/styles/CommonStyle";
+import { Input, Label, PageContainer, PageTitleH1 } from "../assets/styles/CommonStyle";
 import { useEffect } from "react";
 import { useUserDataStore } from "../utils/stores/userStore";
 import { LayoutButtonProps } from "../components/BottomButtonLayout";
@@ -39,49 +39,55 @@ const Login = () => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <InputContainer>
-        <Label htmlFor="id">
-          아이디
-          <Input
-            {...register("id", {
-              required: { value: true, message: "아이디를 입력해주세요" },
-              pattern: {
-                value: /[A-Za-z]+$/,
-                message: "아이디 형식이 올바르지 않습니다",
-              },
-            })}
-            type="text"
-            placeholder="아이디"
-            id="id"
-          />
-          {errors.id && <span>{errors.id.message}</span>}
-        </Label>
+    <>
+      <PageContainer>
+        <PageTitleH1>로그인을 진행해주세요</PageTitleH1>
+      </PageContainer>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputContainer>
+          <Label htmlFor="id">
+            아이디
+            <Input
+              {...register("id", {
+                required: { value: true, message: "아이디를 입력해주세요" },
+                pattern: {
+                  value: /[A-Za-z]+$/,
+                  message: "아이디 형식이 올바르지 않습니다",
+                },
+              })}
+              type="text"
+              placeholder="아이디"
+              id="id"
+            />
+            {errors.id && <span>{errors.id.message}</span>}
+          </Label>
 
-        <Label htmlFor="pwd">
-          비밀번호
-          <Input
-            {...register("pwd", {
-              required: { value: true, message: "비밀번호를 입력해주세요" },
-              minLength: {
-                value: 6,
-                message: "비밀번호는 최소 6자 이상이어야 합니다",
-              },
-            })}
-            type="password"
-            placeholder="비밀번호"
-            id="pwd"
-          />
-          {errors.pwd && <span>{errors.pwd.message}</span>}
-        </Label>
-        <EtcButtonContainer>
-          <RecoveryContainer>
-            <Link to={"/account_recovery/id"}>아이디</Link>/<Link to={"/account_recovery/pwd"}>비밀번호 찾기</Link>
-          </RecoveryContainer>
-          <Link to={"/register"}>회원가입</Link>
-        </EtcButtonContainer>
-      </InputContainer>
-    </Form>
+          <Label htmlFor="pwd">
+            비밀번호
+            <Input
+              {...register("pwd", {
+                required: { value: true, message: "비밀번호를 입력해주세요" },
+                minLength: {
+                  value: 6,
+                  message: "비밀번호는 최소 6자 이상이어야 합니다",
+                },
+              })}
+              type="password"
+              placeholder="비밀번호"
+              id="pwd"
+            />
+            {errors.pwd && <span>{errors.pwd.message}</span>}
+          </Label>
+          <EtcButtonContainer>
+            <RecoveryContainer>
+              <Link to={"/account_recovery/id"}>아이디</Link>/<Link to={"/account_recovery/pwd"}>비밀번호 찾기</Link>
+            </RecoveryContainer>
+            <Link to={"/register"}>회원가입</Link>
+          </EtcButtonContainer>
+        </InputContainer>
+        <Link to={"/write"}>테스트이동</Link>
+      </Form>
+    </>
   );
 };
 
