f849277f3a35a3086a64c4212636466e284d7063
yugyusang <yugyusang12@gmail.com>
2024-09-07 17:28:13 +0900

    feat: 웹뷰여부 체크함수 정의

--- src/utils/functions/commons.ts
+++ src/utils/functions/commons.ts
@@ -1,9 +1,9 @@
 import { FieldErrors, FieldValues } from "react-hook-form";
 
 /**
- * @description react-hook-form의 errors 객체를 받아서 에러메세지를 리턴하는 함수
+ * react-hook-form의 errors 객체를 받아서 에러메세지를 리턴하는 함수
  * @param errors react-hook-form의 errors 객체
- * @returns string 에러메세지
+ * @returns {string} 에러메세지
  */
 export const getErrors = <T extends FieldValues>(errors: FieldErrors<T>) => {
   let error = "";
@@ -14,9 +14,9 @@ export const getErrors = <T extends FieldValues>(errors: FieldErrors<T>) => {
 };
 
 /**
- * @Description 현재 디바이스가 모바일인지 확인하는 함수
+ * 현재 디바이스가 모바일인지 확인하는 함수
  *
- * @returns boolean
+ * @returns {boolean}
  */
 export const isMobile = () => {
   const userAgent = navigator.userAgent;
@@ -24,11 +24,29 @@ export const isMobile = () => {
 };
 
 /**
- * @Description 현재 디바이스가 애플기기인지 확인하는 함수
+ * 현재 디바이스가 애플기기인지 확인하는 함수
  *
- * @returns boolean
+ * @returns {boolean}
  */
 export const isApple = () => {
   const userAgent = navigator.userAgent;
   return /iPad|iPhone|iPod/i.test(userAgent);
+};
+
+/**
+ * 현재 디바이스가 웹뷰인지 확인하는 함수
+ *
+ * @returns {boolean}
+ */
+export const isWebView = () => {
+  const userAgent = navigator.userAgent || navigator.vendor;
+  // iOS 웹뷰 확인
+  if (/iPhone|iPod|iPad/.test(userAgent) && /AppleWebKit/.test(userAgent) && !/Safari/.test(userAgent)) {
+    return true;
+  }
+  // Android 웹뷰 확인
+  if (/Android/.test(userAgent) && /wv/.test(userAgent)) {
+    return true;
+  }
+  return false;
 };
