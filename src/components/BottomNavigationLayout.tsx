bfab43180d99da97e9fb1f68834c2e16d6e954ef
yugyusang <yugyusang12@gmail.com>
2024-09-07 17:31:41 +0900

    feat: 특정 라우터 경로에 별도의 패딩값을 갖도록 수정

--- src/components/BottomNavigationLayout.tsx
+++ src/components/BottomNavigationLayout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom"; //child route를 렌더링하는 컴포넌트
+import { Outlet, useLocation } from "react-router-dom"; //child route를 렌더링하는 컴포넌트
 import BottomNavigationBar from "./BottomNavigationBar";
 import styled from "styled-components";
 
 const BottomNavigationLayout = () => {
+  const location = useLocation();
+
   return (
     <>
-      <Container>
+      <Container $location={location.pathname}>
         <Outlet />
       </Container>
       <BottomNavigationBar />
@@ -15,9 +17,9 @@ const BottomNavigationLayout = () => {
 
 export default BottomNavigationLayout;
 
-const Container = styled.div`
-  padding-bottom: 60px; //하단 네비게이션 바를 위한 여백
+const Container = styled.div<{ $location: string }>`
   width: 100%;
   height: 100%;
-  padding: 20px;
+  padding: ${(props) => (props.$location === "/location" ? "0" : "20px")};
+  padding-bottom: 60px; //하단 네비게이션 바를 위한 여백
 `;
