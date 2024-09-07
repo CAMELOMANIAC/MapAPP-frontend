93e23364aa2bb48dce91ef62e47ddc7535f17acc
yugyusang <yugyusang12@gmail.com>
2024-09-07 17:30:44 +0900

    refactor: 사진 찍는 함수를 컴포넌트 외부에 정의 및 useCompassData 적용

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -10,6 +10,7 @@ import { createPortal } from "react-dom";
 import { IoMdCamera } from "react-icons/io";
 import { AiFillPicture } from "react-icons/ai";
 import EXIF from "exif-js";
+import useCompassData from "../utils/hooks/useCompassData";
 
 type FormType = {
   content: string;
@@ -19,6 +20,8 @@ const Write = () => {
   const [photo, setPhoto] = useState<string | null>(null); //base64로 인코딩된 이미지
   const [alertMessage, setAlertMessage] = useState("");
   const exifRef = useRef();
+  const heading = useCompassData();
+  const [photoDirection, setPhotoDirection] = useState<number>();
 
   const {
     register,
@@ -30,52 +33,9 @@ const Write = () => {
     if (photo) {
       data.photo = photo;
       //이미지가 있으면 data에 photo라는 키로 base64로 인코딩된 이미지를 추가(일반적이라면 이미지를 이미지 서버에 저장하고 그 경로를 업로드 해야하지만 지금은 base64로 인코딩된 이미지 문자열 업로드)
-      console.log("Form Data:", data);
+      console.log("Form Data:", data, "사진방향", photoDirection);
     } else {
       setAlertMessage("이미지를 업로드 해야합니다");
-    }
-  };
-
-  //카메라로 사진 찍기
-  const takePhoto = async () => {
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Camera, // 기본 카메라앱 실행
-    });
-
-    image.dataUrl && setPhoto(image.dataUrl);
-    // 실제 이미지 파일 경로 반환시
-    // const image = await Camera.getPhoto({
-    //   quality: 90,
-    //   allowEditing: false,
-    //   resultType: CameraResultType.Uri, // 실제 이미지 파일 경로 반환
-    //   source: CameraSource.Camera,
-    // });
-
-    // console.log("Image Path:", image.path); // 실제 이미지 파일 경로
-  };
-
-  //갤러리에서 사진 선택
-  const choosePhoto = async () => {
-    try {
-      const image = await Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: CameraResultType.DataUrl,
-        source: CameraSource.Photos, // 갤러리에서 사진 선택
-      });
-
-      image.dataUrl && setPhoto(image.dataUrl);
-    } catch (error) {
-      if (error instanceof Error && error.message === "User cancelled photos app") {
-        if (!isMobile) {
-          //웹에서 사진 선택 취소시 오류를 던지는데, 웹에서는 몇가지 기능이 없어서 오류를 던지지만 기능상 상관없기 때문에 비워놓음
-        }
-      } else {
-        console.error(error);
-      }
     }
   };
 
@@ -84,16 +44,15 @@ const Write = () => {
       // EXIF 데이터 읽기
       const img = new Image();
       img.src = photo;
+      setPhotoDirection(heading);
       img.onload = () => {
         EXIF.getData(photo, (el: HTMLImageElement) => {
           const allMetaData = EXIF.getAllTags(el);
           exifRef.current = allMetaData;
-          console.log("모든태그", allMetaData.exif);
-          console.log("이쁘게", EXIF.pretty(el));
         });
       };
     }
-  }, [photo]);
+  }, [photo]); // eslint-disable-line react-hooks/exhaustive-deps
 
   //모달 관리 커스텀후크
   const { closeModal, isOpen, openModal } = useAlertModal();
@@ -113,6 +72,14 @@ const Write = () => {
       openModal();
     }
   }, [alertMessage, openModal]);
+
+  //takePhoto 함수는 비동기함수이므로 이벤트 핸들러로 사용할때는 함수를 한번 더 감싸줘야함
+  const handleTakePhoto = () => {
+    takePhoto(setPhoto);
+  };
+  const handleChoosePhoto = () => {
+    choosePhoto(setPhoto);
+  };
 
   return (
     <PageContainer>
@@ -137,12 +104,12 @@ const Write = () => {
         {photo && <img src={photo} alt="pick_image" />}
         <PhotoButtonContainer>
           {isMobile() && (
-            <TakePhotoButton onClick={takePhoto}>
+            <TakePhotoButton onClick={handleTakePhoto}>
               카메라
               <IoMdCamera />
             </TakePhotoButton>
           )}
-          <ChoosePhotoButton onClick={choosePhoto}>
+          <ChoosePhotoButton onClick={handleChoosePhoto}>
             사진 선택
             <AiFillPicture />
           </ChoosePhotoButton>
@@ -198,3 +165,48 @@ const TakePhotoButton = styled.button`
 const ChoosePhotoButton = styled.button`
   font-size: 2rem;
 `;
+
+//카메라로 사진 찍기
+const takePhoto = async (setPhoto: (dataUrl: string) => void) => {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.DataUrl,
+    source: CameraSource.Camera, // 기본 카메라앱 실행
+  });
+
+  if (image.dataUrl) {
+    setPhoto(image.dataUrl);
+  }
+  // 실제 이미지 파일 경로 반환시
+  // const image = await Camera.getPhoto({
+  //   quality: 90,
+  //   allowEditing: false,
+  //   resultType: CameraResultType.Uri, // 실제 이미지 파일 경로 반환
+  //   source: CameraSource.Camera,
+  // });
+
+  // console.log("Image Path:", image.path); // 실제 이미지 파일 경로
+};
+
+//갤러리에서 사진 선택
+const choosePhoto = async (setPhoto: (dataUrl: string) => void) => {
+  try {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Photos, // 갤러리에서 사진 선택
+    });
+
+    image.dataUrl && setPhoto(image.dataUrl);
+  } catch (error) {
+    if (error instanceof Error && error.message === "User cancelled photos app") {
+      if (!isMobile) {
+        //웹에서 사진 선택 취소시 오류를 던지는데, 웹에서는 몇가지 기능이 없어서 오류를 던지지만 기능상 상관없기 때문에 비워놓음
+      }
+    } else {
+      console.error(error);
+    }
+  }
+};
