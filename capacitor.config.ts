7403dbd42e28bf112b8c4cc2048c819e55212fce
yugyusang <yugyusang12@gmail.com>
2024-09-08 23:49:12 +0900

    chore: 스플래시 스크린 및 앱 아이콘 설정을 위한 capacitor/splash-screen,@capacitor/assets 플러그인 설치

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,19 @@ const config: CapacitorConfig = {
   appId: "com.camelomaniac.chat",
   appName: "henmy-front",
   webDir: "build",
+  backgroundColor: "#ffffff",
   ios: {
     contentInset: "never",
-    backgroundColor: "#ffffff",
     allowsLinkPreview: false,
+  },
+  plugins: {
+    SplashScreen: {
+      backgroundColor: "#FFFFFF", // 원하는 배경색 코드
+      darkModeBackgroundColor: "#FFFFFF", // 다크 모드 배경색
+      splashFullScreen: true,
+      splashImmersive: true,
+      androidScaleType: "CENTER_CROP",
+    },
   },
 };
 
