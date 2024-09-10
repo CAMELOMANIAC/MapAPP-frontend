006e6db9e47e118cafc1b99c3170c74955b1ae11
yugyusang <yugyusang12@gmail.com>
2024-09-10 18:06:15 +0900

    chore: 백그라운드에서 로컬알림을 보내기위한 캐퍼시터플러그인 설치 및 설정 적용(@capacitor/app, local-notifications, background-task)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,9 @@ const config: CapacitorConfig = {
     contentInset: "never",
     allowsLinkPreview: false,
   },
+  android: {
+    //useLegacyBridge: true, //레거시 브릿지(신형 브릿지에서는 배터리 최적화문제로 웹뷰 통신시 백그라운드 시간을 연장하지 못하는 문제가 있으므로 문제 발생시 사용)
+  },
   plugins: {
     SplashScreen: {
       backgroundColor: "#FFFFFF", // 원하는 배경색 코드
