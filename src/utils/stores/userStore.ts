6be3e9ad939aaa3fd7acf9c1d5233edbde3c7921
yugyusang <yugyusang12@gmail.com>
2024-10-24 18:10:22 +0900

    feat: 중복 타입 정의

--- src/utils/stores/userStore.ts
+++ src/utils/stores/userStore.ts
@@ -1,12 +1,18 @@
 import { create } from "zustand";
 
-type StoreType = {
+export type LocationType = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+export type StoreType = {
   userName: string;
   setUserName: (name: string) => void;
-  location: { latitude: number; longitude: number; zoom: number };
-  setLocation: (location: { latitude: number; longitude: number; zoom: number }) => void;
-  mapCenter: { latitude: number; longitude: number; zoom: number };
-  setMapCenter: (location: { latitude: number; longitude: number; zoom: number }) => void;
+  location: LocationType;
+  setLocation: (location: LocationType) => void;
+  mapCenter: LocationType;
+  setMapCenter: (location: LocationType) => void;
 };
 
 /**
@@ -16,7 +22,7 @@ export const useUserDataStore = create<StoreType>((set) => ({
   userName: "",
   setUserName: (name: string) => set({ userName: name }),
   location: { latitude: 37.5326, longitude: 127.024612, zoom: 14 },
-  setLocation: (location: { latitude: number; longitude: number; zoom: number }) => set({ location }),
+  setLocation: (location: LocationType) => set({ location }),
   mapCenter: { latitude: 37.5326, longitude: 127.024612, zoom: 14 },
-  setMapCenter: (location: { latitude: number; longitude: number; zoom: number }) => set({ mapCenter: location }),
+  setMapCenter: (location: LocationType) => set({ mapCenter: location }),
 }));
