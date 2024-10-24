b7f85ec67c95a9627a61ced0fe676bd48e648159
yugyusang <yugyusang12@gmail.com>
2024-10-24 18:09:23 +0900

    refactor: 데이터 관련 후크 분리

--- src/utils/hooks/useGooglePlaceData.ts
+++ src/utils/hooks/useGooglePlaceData.ts
@@ -1,12 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
 /**
  * 구글 플레이스 API를 이용하여 주변의 명소 데이터를 가져옵니다.
- *
- * @async
- * @param {number} latitude
- * @param {number} logtitude
- * @returns {unknown}
+ * @param {number} latitude - 위도
+ * @param {number} longitude - 경도
+ * @returns {Object} data - 명소 데이터
  */
-export const getGooglePlaceData = async (latitude: number, longitude: number) => {
+const useGooglePlaceData = (latitude: number, longitude: number) => {
+  const { data, isSuccess } = useQuery({
+    queryKey: ["googlePlaceData", latitude, longitude],
+    queryFn: () => getGooglePlaceData(latitude, longitude),
+    staleTime: 1000 * 60,
+  });
+  return { data, isSuccess };
+};
+
+export default useGooglePlaceData;
+
+const getGooglePlaceData = async (latitude: number, longitude: number) => {
   const API_URL = "https://places.googleapis.com/v1/places:searchNearby";
   const FIELD_MASK = "places.displayName,places.formattedAddress,places.photos,places.location";
   const headers = new Headers({
