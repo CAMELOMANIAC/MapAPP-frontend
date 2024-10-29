bbdc45a2f12a3ead3f08ba9208060d2548beeadd
yugyusang <yugyusang12@gmail.com>
2024-10-29 23:56:07 +0900

    style: 컴포넌트 배경색 변경

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -35,7 +35,7 @@ const Container = styled.div`
   bottom: 0;
   width: 100%;
   height: 60px;
-  background-color: #f8f8f8;
+  background-color: white;
   display: flex;
   justify-content: space-around;
   align-items: center;
