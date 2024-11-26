5de9297d26f8605318fb7e51e60a5a2a655713bd
CAMELOMANIAC <yugyusang12@gmail.com>
2024-11-26 22:56:16 +0900

    feat: 드래그가능한 마커와 글쓰기 버튼 추가

---
+++ src/components/MarkerDraggableButton.tsx
@@ -0,0 +1,57 @@
+import { ButtonHTMLAttributes } from "react";
+import { MdOutlineAddLocation } from "react-icons/md";
+import { IoMdMove } from "react-icons/io";
+import styled from "styled-components";
+
+type MarkerDraggableButtonProps = {} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const MarkerDraggableButton = ({ ...props }: MarkerDraggableButtonProps) => {
+  return (
+    <ButtonContainer>
+      <Button {...props}>
+        <MdOutlineAddLocation />
+      </Button>
+      <DraggableIcon>
+        <IoMdMove />
+      </DraggableIcon>
+    </ButtonContainer>
+  );
+};
+
+export default MarkerDraggableButton;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  padding: 10px;
+  font-size: 3rem;
+  color: var(--thema-color);
+  cursor: grab;
+  background-color: white;
+  border-radius: 100%;
+  box-shadow: 0 0 10px 0 rgb(0 0 0 / 30%);
+  -webkit-user-drag: none;
+  -moz-window-dragging: no-drag;
+`;
+
+const DraggableIcon = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  font-size: 1.2rem;
+  color: white;
+  background-color: var(--thema-color);
+  border-radius: 100%;
+  transform: translate(-100%, -100%);
+  -webkit-user-drag: none;
+`;
--- src/components/WriteButton.tsx
+++ src/components/WriteButton.tsx
@@ -1,68 +1,39 @@
-import { ButtonHTMLAttributes, useState } from "react";
-import { MdOutlineAddLocation } from "react-icons/md";
-import { IoMdMove } from "react-icons/io";
 import styled from "styled-components";
+import { Link, LinkProps } from "react-router-dom";
 
-type WriteButtonProps = {} & ButtonHTMLAttributes<HTMLButtonElement>;
+type WriteButtonProps = { isVisible: boolean } & LinkProps;
 
-const WriteButton = ({ ...props }: WriteButtonProps) => {
+const WriteButton = ({ isVisible, ...props }: WriteButtonProps) => {
   return (
-    <ButtonContainer>
-      <Button {...props}>
-        <MdOutlineAddLocation />
-      </Button>
-      <DraggableIcon>
-        <IoMdMove />
-      </DraggableIcon>
+    <ButtonContainer $isVisible={isVisible}>
+      <Button {...props}>작성하기</Button>
     </ButtonContainer>
   );
 };
 
 export default WriteButton;
 
-const ButtonContainer = styled.div`
-  position: absolute;
-  width: 100px;
-  height: 100px;
-  left: 50%;
-  transform: translateX(-50%);
-  margin: 0 auto;
-  bottom: 1rem;
-  -webkit-user-drag: none;
-  -moz-window-dragging: no-drag;
-  cursor: "grab";
-`;
-
-const Button = styled.button`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 3rem;
-  background-color: white;
-  border-radius: 100%;
-  padding: 10px;
-  color: var(--thema-color);
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
-  -webkit-user-drag: none;
-  -moz-window-dragging: no-drag;
-  cursor: grab;
-`;
-
-const DraggableIcon = styled.div`
-  position: absolute;
-  font-size: 1.2rem;
-  background-color: var(--thema-color);
-  color: white;
-  border-radius: 100%;
-  top: 0%;
-  left: 0%;
-  width: 24px;
-  height: 24px;
-  transform: translate(50%, 50%);
+const Button = styled(Link)`
+  position: relative;
   display: flex;
-  justify-content: center;
+  flex-wrap: nowrap;
   align-items: center;
-  -webkit-user-drag: none;
-  -moz-window-dragging: no-drag;
+  justify-content: center;
+  width: 100%;
+  height: 3rem;
+  overflow: hidden;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: var(--thema-color);
+  background-color: white;
+  border-radius: 1rem;
+  box-shadow: 0 0 10px 0 rgb(0 0 0 / 30%);
+`;
+
+const ButtonContainer = styled.div<{ $isVisible: boolean }>`
+  position: relative;
+  flex: ${(props) => (props.$isVisible ? 1 : 0)};
+  padding: 10px;
+  overflow: hidden;
+  transition: all 1s;
 `;
