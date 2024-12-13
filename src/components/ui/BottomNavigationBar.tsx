00c466872a7922381be2198df92460394603b639
CAMELOMANIAC <yugyusang12@gmail.com>
2024-12-13 21:27:52 +0900

    style: 특정 페이지에서 네비게이션바가 분리되어 보이도록 라운딩 처리

--- src/components/ui/BottomNavigationBar.tsx
+++ src/components/ui/BottomNavigationBar.tsx
@@ -2,12 +2,14 @@ import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { GoHomeFill } from "react-icons/go";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const BottomNavigationBar = () => {
+  const location = useLocation();
+
   return (
-    <Container>
+    <Container $path={location.pathname}>
       <StyledNavLink to="/">
         <GoHomeFill />
         <p>홈</p>
@@ -30,7 +32,7 @@ const BottomNavigationBar = () => {
 
 export default BottomNavigationBar;
 
-const Container = styled.div`
+const Container = styled.div<{ $path: string }>`
   position: absolute;
   bottom: 0;
   display: flex;
@@ -39,6 +41,12 @@ const Container = styled.div`
   width: 100%;
   height: 60px;
   background-color: white;
+  border-radius: ${(props) =>
+    props.$path === "/location"
+      ? "0px"
+      : "1.5rem 1.5rem 0 0"}; /* location은 지도라서 글로벌 네비바가 분리되어보이지만 나머지는 그러지 않아서 추가 */
+
+  box-shadow: 0 0 3px rgb(0 0 0 / 10%);
 `;
 
 const StyledNavLink = styled(NavLink)`
