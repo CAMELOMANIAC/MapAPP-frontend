4a5655c696b57864243a8af166cea94327501ef4
CAMELOMANIAC <yugyusang12@gmail.com>
2024-12-13 21:27:23 +0900

    feat: 특정 페이지에서 하단글로벌 네비게이션 바를 숨기는 기능 추가

--- src/components/layouts/BottomNavigationLayout.tsx
+++ src/components/layouts/BottomNavigationLayout.tsx
@@ -5,24 +5,26 @@ import BottomNavigationBar from "../ui/BottomNavigationBar";
 
 type BottomNavigationLayoutProps = {
   xMargin?: string;
+  isNavHidden?: boolean;
 };
-const BottomNavigationLayout = ({ xMargin }: BottomNavigationLayoutProps) => {
+const BottomNavigationLayout = ({ xMargin, isNavHidden }: BottomNavigationLayoutProps) => {
   return (
     <>
-      <Container $xMargin={xMargin}>
+      <Container $xMargin={xMargin} $isNavHidden={isNavHidden}>
         <Outlet />
       </Container>
-      <BottomNavigationBar />
+      {!isNavHidden && <BottomNavigationBar />}
     </>
   );
 };
 
 export default BottomNavigationLayout;
 
-const Container = styled.div<{ $xMargin: string | undefined }>`
+const Container = styled.div<{ $xMargin: string | undefined; $isNavHidden: boolean | undefined }>`
   width: 100%;
   height: 100%;
   padding: ${(props) => (props.$xMargin ? props.$xMargin : "20px")};
-  padding-bottom: 60px; /* 하단 네비게이션 바를 위한 여백 */
+  padding-bottom: ${(props) => (props.$isNavHidden ? "0" : "60px")}; /* 하단 네비게이션 바를 위한 여백 */
+  overflow-y: auto;
   background-color: #f8f8f8;
 `;
