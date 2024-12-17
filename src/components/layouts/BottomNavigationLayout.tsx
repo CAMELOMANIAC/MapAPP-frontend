41ccd4f36afd26f02f8ca4db494ae9d85a5b8bf1
CAMELOMANIAC <yugyusang12@gmail.com>
2024-12-18 02:03:17 +0900

    feat: 페이지 전환 애니메이션 추가

--- src/components/layouts/BottomNavigationLayout.tsx
+++ src/components/layouts/BottomNavigationLayout.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from "react-router-dom"; //child route를 렌더링하는 컴포넌트
+import { AnimatePresence, motion } from "framer-motion";
+import { Outlet, useLocation } from "react-router-dom"; //child route를 렌더링하는 컴포넌트
 import styled from "styled-components";
 
 import BottomNavigationBar from "../ui/BottomNavigationBar";
+import LoadingScreen from "../ui/LoadingScreen";
 
 type BottomNavigationLayoutProps = {
   xMargin?: string;
   isNavHidden?: boolean;
 };
 const BottomNavigationLayout = ({ xMargin, isNavHidden }: BottomNavigationLayoutProps) => {
+  const location = useLocation();
+
   return (
     <>
-      <Container $xMargin={xMargin} $isNavHidden={isNavHidden}>
-        <Outlet />
-      </Container>
-      {!isNavHidden && <BottomNavigationBar />}
+      <LoadingScreen></LoadingScreen>
+      <AnimatePresence>
+        <motion.div exit={{ opacity: 1 }} animate={{ opacity: 1 }} initial={{ opacity: 0 }} key={location.pathname}>
+          <Container $xMargin={xMargin} $isNavHidden={isNavHidden}>
+            <Outlet />
+          </Container>
+        </motion.div>
+        {!isNavHidden && <BottomNavigationBar />}
+      </AnimatePresence>
     </>
   );
 };
@@ -21,6 +30,8 @@ const BottomNavigationLayout = ({ xMargin, isNavHidden }: BottomNavigationLayout
 export default BottomNavigationLayout;
 
 const Container = styled.div<{ $xMargin: string | undefined; $isNavHidden: boolean | undefined }>`
+  position: absolute;
+  bottom: 0;
   width: 100%;
   height: 100%;
   padding: ${(props) => (props.$xMargin ? props.$xMargin : "20px")};
