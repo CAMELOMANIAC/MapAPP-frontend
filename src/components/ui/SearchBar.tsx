d57d8b791d8676b2303e37a6618d6700210da965
CAMELOMANIAC <yugyusang12@gmail.com>
2024-12-18 22:17:27 +0900

    feat: 검색바 클릭시 검색페이지로 이동하도록 수정

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,24 +1,48 @@
-import { HTMLAttributes } from 'react';
-import styled from 'styled-components';
+import { HTMLAttributes } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 type SearchBarProps = {} & HTMLAttributes<HTMLDivElement>;
 
 const SearchBar = ({ ...props }: SearchBarProps) => {
+  const navigate = useNavigate();
+  const searchBarClickHandler = () => {
+    navigate("/search");
+  };
   return (
-    <SearchBarInput type="text" placeholder="검색어를 입력하세요" {...props} />
+    <SearchContainer onClick={searchBarClickHandler}>
+      <SearchIcon />
+      <SearchBarInput type="text" placeholder="검색어를 입력하세요" {...props} />
+    </SearchContainer>
   );
 };
 
 export default SearchBar;
 
-const SearchBarInput = styled.input`
+const SearchContainer = styled.div`
   display: flex;
-  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
   width: 100%;
   padding: 10px;
   font-size: 16px;
   background-color: white;
-  border: none;
   border-radius: 10px;
   box-shadow: 0 0 10px rgb(0 0 0 / 10%);
+`;
+
+const SearchBarInput = styled.input`
+  display: flex;
+  width: 100%;
+  padding-left: 0.5rem;
+  font-size: 16px;
+  border: none;
+  outline: none;
+`;
+
+const SearchIcon = styled(FaSearch)`
+  margin: 0 0.4rem;
+  color: #6e6e6e;
 `;
